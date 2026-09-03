695bf870fc610eb12db0927bcbcc48ae2e0314fd
Atharva_Ramawat <atharvaramawat027@gmail.com>
2026-09-03 19:50:25 +0530

    Fix typo in Safe Write Test echo statement

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-echo "=== Running Safe Write Test ="
+echo "=== Running Safe Write Test =="
 docker exec -e ETCDCTL_API=3 etcd-legacy etcdctl --endpoints=http://envoy:9090 --insecure-transport=true put proxy "Working"
 
 echo "=== Running Blocked Exploit Test ==="
